84ce74cdc32a9a40d76afde205245906ea4b19d7
Kartik Kumar <85959032+Kartik7906@users.noreply.github.com>
2024-09-30 21:45:53 +0530

    connection change in client

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -1,23 +1,30 @@
-import React, { createContext, useContext, useMemo } from "react";
+import React, { createContext, useContext, useMemo, useEffect } from "react";
 import { io } from "socket.io-client";
 
 const SocketContext = createContext(null);
 
-
-export const useSocket = () =>{
+export const useSocket = () => {
     const socket = useContext(SocketContext);
+    if (!socket) {
+        throw new Error("useSocket must be used within a SocketProvider");
+    }
     return socket;
 }
 
+const SocketProvider = ({ children }) => {
+    const socket = useMemo(() => io("https://vision-meet-server.vercel.app/"), []);
 
-const SocketProvider = (props) => {
-  const socket = useMemo(() => io("https://vision-meet-server.vercel.app/"), []);
+    useEffect(() => {
+        return () => {
+            socket.disconnect(); // Clean up the socket connection on unmount
+        };
+    }, [socket]);
 
-  return (
-    <SocketContext.Provider value={socket}>
-      {props.children}
-    </SocketContext.Provider>
-  );
+    return (
+        <SocketContext.Provider value={socket}>
+            {children}
+        </SocketContext.Provider>
+    );
 };
 
 export default SocketProvider;
